05c75ce92aa5be32d6194fd8b2ff1e97c2bca0ff
Wallinator <40655366+Wallinator@users.noreply.github.com>
2020-03-30 20:03:36 +1100

    extracted methods from StartTimer

--- Services/TimerService.cs
+++ Services/TimerService.cs
@@ -8,30 +8,39 @@ namespace WerewolfOnline.Services {
 	public static class TimerService {
 		public static CancellationTokenSource StartTimer(int timerMs, Action OnCancel, Action OnTimeOut) {
 
-			var source = new CancellationTokenSource();
+			CancellationTokenSource source = new CancellationTokenSource();
+			if (timerMs > 0) {
+				StartTimerAwaitingCancel(timerMs, OnCancel, OnTimeOut, source);
+			}
+			else {
+				StartAwaitingCancel(OnCancel, source);
+			}
+			return source;
+		}
+
+		private static void StartAwaitingCancel(Action OnCancel, CancellationTokenSource source) {
 			_ = Task.Run(() => {
-				if (timerMs > 0) {
-					Thread.Sleep(timerMs);
+				while (true) {
 					if (source.Token.IsCancellationRequested) {
 						OnCancel();
 						return;
 					}
-					else {
-						OnTimeOut();
-						return;
-					}
-				}
-
-				else {
-					while (true) {
-						if (source.Token.IsCancellationRequested) {
-							OnCancel();
-							return;
-						}
-					}
 				}
 			});
-			return source;
+		}
+
+		private static void StartTimerAwaitingCancel(int timerMs, Action OnCancel, Action OnTimeOut, CancellationTokenSource source) {
+			_ = Task.Run(() => {
+				Thread.Sleep(timerMs);
+				if (source.Token.IsCancellationRequested) {
+					OnCancel();
+					return;
+				}
+				else {
+					OnTimeOut();
+					return;
+				}
+			});
 		}
 	}
 }
